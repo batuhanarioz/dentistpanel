3e97aeb4de575c606b4b6bb7f618e6942b1b0cf2
Batuhan Arıöz <batuhanarioz98@gmail.com>
2026-03-03 04:50:20 +0300

    fix: update favicon and explicitly define in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Diş Kliniği Yönetim Paneli - NextGency",
   description: "NextGency ile diş kliniğinizi panelimiz ve otomasyonlarımızla kolayca yönetin.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 import * as Sentry from "@sentry/nextjs";
